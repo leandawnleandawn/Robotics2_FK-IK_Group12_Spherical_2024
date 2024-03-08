3a21049becc2d9756e47828a7b53e9285b4edd30
leandawnleandawn <lyndonallensales@gmail.com>
2024-03-08 11:44:06 +0800

    Edited and Fxed

--- GUI/main.py
+++ GUI/main.py
@@ -105,7 +105,7 @@ class Window(RoboticProgram):
         self.Zdata.grid(row=2, column=1)
         cm7.grid(row=2, column=2)
         
-    def reset(self):
+    def reset(self):        
         self.a1data.delete(0,'end')
         self.a2data.delete(0, 'end')
         self.a3data.delete(0, 'end')
@@ -133,6 +133,8 @@ class FkinWindow(Window):
         forward.grid(row=0, column=0)
         reset = tk.Button(BF, text = "Reset", command=self.reset)
         reset.grid(row=0, column=1)
+        
+        self.robotTB(1,0.5,0.5,0,0,0)
           
     def fkin(self):
         self.Xdata.config(state= tk.NORMAL)
@@ -145,8 +147,8 @@ class FkinWindow(Window):
             a1 = float(self.a1data.get()) / 100
             a2 = float(self.a2data.get()) / 100
             a3 = float(self.a3data.get()) / 100
-            t1 = float(self.T1data.get())
-            t2 = float(self.T2data.get())
+            t1 = float(self.T1data.get()) * (np.pi/180)
+            t2 = float(self.T2data.get()) * (np.pi/180)
             d3 = float(self.d3data.get()) / 100
         except ValueError:
             pop_up = tk.Toplevel(master= robot)
@@ -168,22 +170,36 @@ class FkinWindow(Window):
         result = np.dot(np.dot(htm[0], htm[1]), htm[2])
         print(np.round(result,2))
         
-        self.Xdata.insert(tk.END, np.round(result[0,3],2))
-        self.Ydata.insert(tk.END, np.round(result[1,3],2))
-        self.Zdata.insert(tk.END, np.round(result[2,3],2))
-        
-        self.Xdata.config(state= tk.DISABLED)
-        self.Ydata.config(state= tk.DISABLED)
-        self.Zdata.config(state= tk.DISABLED)
+        if (t1 < -np.pi/2 or t1 > np.pi/2) or (t2 < -np.pi/2 or t2 > np.pi/2) or (d3 < 0 or d3 >=  0.5):
+            another_pop_up = tk.Toplevel(master=robot)
+            qlim_error = tk.Label(another_pop_up, text = "The Calculated Joint Limits exceeded the Acutal Joint Limits")
+            qlim_error.pack()
+        else:
+            
+            self.Xdata.insert(tk.END, np.round(result[0,3],2))
+            self.Ydata.insert(tk.END, np.round(result[1,3],2))
+            self.Zdata.insert(tk.END, np.round(result[2,3],2))
+            self.Xdata.config(state= tk.DISABLED)
+            self.Ydata.config(state= tk.DISABLED)
+            self.Zdata.config(state= tk.DISABLED)
+            self.robotTB(a1, a2, a3, t1, t2, d3)
         
     def dhMatrix(self, theta, alpha, radius, distance):
-    	return np.matrix([
-			[np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(theta)*np.sin(alpha), radius*np.cos(theta)],
-        	[np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), radius*np.sin(theta)],
-        	[0, np.sin(alpha), np.cos(alpha), distance],
-        	[0,0,0,1]
-		])
-    
+        return np.matrix([
+            [np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(theta)*np.sin(alpha), radius*np.cos(theta)],
+            [np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), radius*np.sin(theta)],
+            [0, np.sin(alpha), np.cos(alpha), distance],
+            [0,0,0,1]
+        ])
+    def robotTB(self, a1, a2, a3, t1, t2, d3):
+        
+        H01 = RevoluteDH(a1, 0, np.pi/2, 0, [-np.pi/2, np.pi/2])
+        H12 = RevoluteDH(0, 0, np.pi/2, np.pi/2, [-np.pi/2, np.pi/2])
+        H23 = PrismaticDH(0,0,0,a2+a3,[0, 0.5])
+        
+        sphericalManipulator = SerialLink([H01, H12, H23])
+        sphericalManipulator.plot([t1, t2, d3])
+
 class IkinWindow(Window):
     def __init__(self):
         super().__init__()
@@ -221,13 +237,16 @@ class IkinWindow(Window):
             pop_up = tk.Toplevel(master= robot)
             label = tk.Label(pop_up, text = "Use the approriate syntax (float)")
             label.pack()
+        try:
+            t1, t2, d3 = self.invKins(a1, a2, a3, x, y, z)
+        except ZeroDivisionError:
+            pop_up = tk.Toplevel(master= robot)
+            label = tk.Label(pop_up, text = "The Inverse Kinematic Calculation is undefined")
+            label.pack()
             
-        t1, t2, d3 = self.invKins(a1, a2, a3, x, y, z)
-        
         self.T1data.insert(tk.END, t1)
         self.T2data.insert(tk.END, t2)
         self.d3data.insert(tk.END, d3)
-        
         self.T1data.config(state= tk.DISABLED)
         self.T2data.config(state= tk.DISABLED)
         self.d3data.config(state= tk.DISABLED)
@@ -237,7 +256,7 @@ class IkinWindow(Window):
         r = np.sqrt((x_03**2) + (y_03**2))
         theta1 = np.arctan(y_03/x_03) * 180/np.pi
         theta2 = np.arctan(s/r) * 180/np.pi
-        d3 = np.sqrt((r**2) + (s**2)) - a2 - a3
+        d3 = (np.sqrt((r**2) + (s**2)) - a2 - a3) * 100
         return theta1, theta2, d3
 
 
