241ee2b45417dd6d843eaed92382b16056547a5b
leandawnleandawn <lyndonallensales@gmail.com>
2024-03-28 21:09:24 +0800

    Fixed

--- Model of Spherical Manipulator/Model.py
+++ Model of Spherical Manipulator/Model.py
@@ -2,10 +2,13 @@ import math
 from roboticstoolbox import SerialLink, PrismaticDH, RevoluteDH
 
 
-a1 = 0.3 # 300mm
-a2 = 0.15 # 300mm
-a3 = 0.15 # 500mm
+def defineLinks():
+    a1 = float(input("a_1 Link [mm]>>>"))
+    a2 = float(input("a_2 Link [mm]>>>"))
+    a3 = float(input("a_3 Link [mm]>>>"))
+    return a1, a2, a3
 
+a1, a2, a3 = defineLinks()
 
 H01 = RevoluteDH(a1, 0, math.pi/2, 0, [-math.pi/2, math.pi/2])
 H12 = RevoluteDH(0, 0, math.pi/2, math.pi/2, [-math.pi/2, math.pi/2])
