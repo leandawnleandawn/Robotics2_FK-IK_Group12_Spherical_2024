b9f31c5117099cdf7aff08786240caf664d5c799
Dianne Tricia Mae Ortega <157595798+srvntwrrior@users.noreply.github.com>
2024-04-05 21:38:42 +0800

    Update improved_main.py

--- GUI/improved_main.py
+++ GUI/improved_main.py
@@ -313,7 +313,5 @@ class JBinWindow():
 robot = RoboticProgram()
 robot.style.theme_use('simplex')
 
-default_font = nametofont('TkDefaultFont')
-default_font.configure(family="Helvetica", size=12)
-robot.mainloop()
+
 
